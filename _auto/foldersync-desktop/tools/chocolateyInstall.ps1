16d2fa1d0d2027aac4ed770cc7f34f6c510eedfa
mu88 <4560672+mu88@users.noreply.github.com>
2025-01-10 16:53:18 +0100

    feat(foldersync-desktop): use DISM to install app for all users

--- _auto/foldersync-desktop/tools/chocolateyInstall.ps1
+++ _auto/foldersync-desktop/tools/chocolateyInstall.ps1
@@ -11,8 +11,8 @@ $checksumType = 'SHA256'
 # Download installer package and verify checksum
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url64bit $url64 -Checksum64 $checksum -ChecksumType64 $checksumType
 
-# Install the package
-Add-AppxPackage -Path $fileFullPath
+# Install the package for all users (that's why we use DISM instead of Add-AppxPackage)
+DISM.EXE /Online /Add-ProvisionedAppxPackage /PackagePath:$fileFullPath /SkipLicense
 
 # Disable package auto updates as otherwise Chocolatey might get confused
 $packageFamilyName = (Get-AppxPackage -AllUsers | Where-Object { $_.Name -eq 'FoldersyncDesktop' }).PackageFamilyName

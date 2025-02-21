8daed852f84625ece5b791d5123c8d7df6cd2972
mu88 <mu88@users.noreply.github.com>
2025-02-21 16:13:49 +0000

    feat(foldersync-desktop): upgrade to v2.2.0

--- _auto/foldersync-desktop/tools/chocolateyInstall.ps1
+++ _auto/foldersync-desktop/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName  = 'foldersync-desktop'
-$url64        = 'https://github.com/tacitdynamics/foldersync-desktop-production/releases/download/2.0.3/foldersync-desktop-2.0.3.x64.msix'
+$url64        = 'https://github.com/tacitdynamics/foldersync-desktop-production/releases/download/2.2.0/foldersync-desktop-2.2.0.x64.msix'
 $toolsDir     = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $fileName     = Split-Path -Path $url64 -Leaf
 $fileFullPath = Join-Path $toolsDir $fileName
-$checksum     = '1382B247EFB2DCB964500B23804067B94F3BC4CD617557448013A54F0154CDDC'
-$checksumType = 'SHA256'
+$checksum     = '121a22e8c0e9906c2af3cc63bddf0fbddaf0ad966fb9cd9444e38d605604b4c2'
+$checksumType = 'sha256'
 
 # Download installer package and verify checksum
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url64bit $url64 -Checksum64 $checksum -ChecksumType64 $checksumType

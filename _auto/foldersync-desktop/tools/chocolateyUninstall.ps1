5c73107d1ea500827a39884836d91172b964cfb9
mu88 <4560672+mu88@users.noreply.github.com>
2025-08-02 15:37:57 +0200

    fix(foldersync-desktop): ensure removal of FoldersyncDesktop package for all users during uninstallation

--- _auto/foldersync-desktop/tools/chocolateyUninstall.ps1
+++ _auto/foldersync-desktop/tools/chocolateyUninstall.ps1
@@ -1,4 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageFullName = (Get-AppxPackage -AllUsers | Where-Object { $_.Name -eq 'FoldersyncDesktop' }).PackageFullName
-Remove-AppxPackage -Package $packageFullName
+Remove-AppxPackage -Package $packageFullName -AllUsers

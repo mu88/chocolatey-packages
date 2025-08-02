d3d3807f2907ad154407ebfba5eeac5c10b6027b
mu88 <4560672+mu88@users.noreply.github.com>
2025-08-02 16:52:29 +0200

    fix(foldersync-desktop): apply temporary workaround as Microsoft broke all *-AppxPackage commands

--- _auto/foldersync-desktop/tools/chocolateyInstall.ps1
+++ _auto/foldersync-desktop/tools/chocolateyInstall.ps1
@@ -14,6 +14,19 @@ Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fileFullPath -Url
 # Install the package for all users (that's why we use DISM instead of Add-AppxPackage)
 DISM.EXE /Online /Add-ProvisionedAppxPackage /PackagePath:$fileFullPath /SkipLicense
 
+# On Windows 11 24H2, Microsoft broke all *-AppxPackage commands. As a temporary workaround, we use the GAC to install some required assemblies.
+Add-Type -AssemblyName "System.EnterpriseServices"
+$publish = [System.EnterpriseServices.Internal.Publish]::new()
+@(
+    'System.Numerics.Vectors.dll',
+    'System.Runtime.CompilerServices.Unsafe.dll',
+    'System.Security.Principal.Windows.dll',
+    'System.Memory.dll'
+) | ForEach-Object {
+    $dllPath = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\$_"
+    $publish.GacInstall($dllPath)
+}
+
 # Disable package auto updates as otherwise Chocolatey might get confused
 $packageFamilyName = (Get-AppxPackage -AllUsers | Where-Object { $_.Name -eq 'FoldersyncDesktop' }).PackageFamilyName
 Remove-AppxPackageAutoUpdateSettings -PackageFamilyName $packageFamilyName -AllUsers
--- _auto/foldersync-desktop/tools/chocolateyUninstall.ps1
+++ _auto/foldersync-desktop/tools/chocolateyUninstall.ps1
@@ -1,4 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+# On Windows 11 24H2, Microsoft broke all *-AppxPackage commands. As a temporary workaround, we use the GAC to install some required assemblies.
+Add-Type -AssemblyName "System.EnterpriseServices"
+$publish = [System.EnterpriseServices.Internal.Publish]::new()
+@(
+    'System.Numerics.Vectors.dll',
+    'System.Runtime.CompilerServices.Unsafe.dll',
+    'System.Security.Principal.Windows.dll',
+    'System.Memory.dll'
+) | ForEach-Object {
+    $dllPath = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\$_"
+    $publish.GacInstall($dllPath)
+}
+
 $packageFullName = (Get-AppxPackage -AllUsers | Where-Object { $_.Name -eq 'FoldersyncDesktop' }).PackageFullName
 Remove-AppxPackage -Package $packageFullName -AllUsers
